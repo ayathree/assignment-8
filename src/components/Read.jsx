f9e26e1ea066889d33ce6a2484e4967b19a15704
ayathree <nobanitaayathree333@gmail.com>
2024-03-28 03:39:01 +0600

    added the responsive booklist section

--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { CiLocationOn } from "react-icons/ci";
+import { IoPeopleOutline } from "react-icons/io5";
+import { MdOutlineFindInPage } from "react-icons/md";
 
 
 const Read = () => {
@@ -16,13 +19,32 @@ const Read = () => {
     return (
         <div>
             {
-                reads.map(read=><div key={read.id} className="card lg:card-side bg-base-100 shadow-xl mt-7">
-                <figure><img src={read.image} alt="Movie"/></figure>
+                reads.map(read=><div key={read.id} className="card lg:card-side bg-base-100 border-2 mt-7 mb-7">
+                <figure><img className="w-[300px] h-[300px] p-5" src={read.image} alt=''/></figure>
                 <div className="card-body">
-                  <h2 className="card-title">New movie is released!</h2>
-                  <p>Click the button to watch on Jetflix app.</p>
-                  <div className="card-actions justify-end">
-                    <Link to={`/card/${read.id}`}><button className="btn btn-primary">Show</button></Link>
+                  <h2 className="card-title font-bold text-2xl">{read.bookName}</h2>
+                  <p className="font-semibold text-lg">By:{read.author}</p>
+                  
+    <div className="flex lg:flex-row flex-col items-center gap-5">
+      <div><p className="font-semibold text-lg">Tags: </p></div>
+    <div className="badge bg-[#59C6D2] p-2 text-white font-semibold ">{read.tags[0]}</div>
+    <div className="badge bg-[#59C6D2] p-2 text-white font-semibold">{read.tags[1]}</div>
+    <CiLocationOn />
+    <div>Publishing Year: {read.yearOfPublishing}</div>
+    </div>
+    <div className="flex lg:flex-row flex-col items-center gap-5">
+    <IoPeopleOutline />
+    <div><p>Publisher: {read.publisher}</p></div>
+    
+    <MdOutlineFindInPage />
+    <p>Page: {read.totalPages}</p>
+    </div>
+    <hr />
+    <br />
+                  <div className="card-actions  flex lg:flex-row flex-col item-center gap-6">
+                    <div><button className="bg-blue-200 text-blue-600 rounded-2xl p-4">Category: {read.category}</button></div>
+                   <div> <button className="bg-orange-100 text-orange-500 rounded-2xl p-4">Ratings: {read.rating}</button></div>
+                  <div><Link to={`/card/${read.id}`}><button className="btn bg-[#23BE0A] text-white rounded-2xl p-4">View Details</button></Link></div>
                   </div>
                 </div>
               </div>)
--- src/components/Wish.jsx
+++ src/components/Wish.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { CiLocationOn } from "react-icons/ci";
+import { IoPeopleOutline } from "react-icons/io5";
+import { MdOutlineFindInPage } from "react-icons/md";
 
 
 const Wish = () => {
@@ -16,21 +19,39 @@ const Wish = () => {
 
 
     return (
-       <div>
-        {
-            wishs.map(wish=> <div key={wish.id}
-            className="card lg:card-side bg-base-100 shadow-xl mt-7">
-            <figure><img src={wish.image} alt="Movie"/></figure>
-            <div className="card-body">
-              <h2 className="card-title">New movie is released!</h2>
-              <p>Click the button to watch on Jetflix app.</p>
-              <div className="card-actions justify-end">
-                <Link to={`/card/${wish.id}`}> <button>Show</button> </Link>
-              </div>
+      <div>
+      {
+          wishs.map(wish=><div key={wish.id} className="card lg:card-side bg-base-100 border-2 mt-7 mb-7">
+          <figure><img className="w-[300px] h-[300px] p-5" src={wish.image} alt=''/></figure>
+          <div className="card-body">
+            <h2 className="card-title font-bold text-2xl">{wish.bookName}</h2>
+            <p className="font-semibold text-lg">By:{wish.author}</p>
+            
+<div className="flex lg:flex-row flex-col items-center gap-5">
+<div><p className="font-semibold text-lg">Tags: </p></div>
+<div className="badge bg-[#59C6D2] p-2 text-white font-semibold ">{wish.tags[0]}</div>
+<div className="badge bg-[#59C6D2] p-2 text-white font-semibold">{wish.tags[1]}</div>
+<CiLocationOn />
+<div>Publishing Year: {wish.yearOfPublishing}</div>
+</div>
+<div className="flex lg:flex-row flex-col items-center gap-5">
+<IoPeopleOutline />
+<div><p>Publisher: {wish.publisher}</p></div>
+
+<MdOutlineFindInPage />
+<p>Page: {wish.totalPages}</p>
+</div>
+<hr />
+<br />
+            <div className="card-actions  flex lg:flex-row flex-col item-center gap-6">
+              <div><button className="bg-blue-200 text-blue-600 rounded-2xl p-4">Category: {wish.category}</button></div>
+             <div> <button className="bg-orange-100 text-orange-500 rounded-2xl p-4">Ratings: {wish.rating}</button></div>
+            <div><Link to={`/card/${wish.id}`}><button className="btn bg-[#23BE0A] text-white rounded-2xl p-4">View Details</button></Link></div>
             </div>
-          </div>)
-        }
-       </div>
+          </div>
+        </div>)
+      }
+  </div>
     );
 };
 
