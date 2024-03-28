1c3e4af6093a804a5db2a91d72b88b1b1433c5d2
ayathree <nobanitaayathree333@gmail.com>
2024-03-28 20:49:30 +0600

    added the screen responsive code

--- src/utilities/localStorage.js
+++ src/utilities/localStorage.js
@@ -1,75 +1,49 @@
 import Swal from 'sweetalert2';
 
-export const saveWishDataToLocalStorage =(card)=>{
-    const saveWishData = JSON.parse(localStorage.getItem('wishs')) ||[];
-    
+export const saveWishDataToLocalStorage = (card) => {
+    const saveWishData = JSON.parse(localStorage.getItem('wishs')) || [];
+    const saveReadData = JSON.parse(localStorage.getItem('reads')) || [];
 
-        const hasWishData = saveWishData.find((item) => item.id == card.id);
+    const hasReadData = saveReadData.find(item => item.id === card.id);
+    if (hasReadData) {
+        Swal.fire({
+            title: " Already Marked as Read",
+            icon: "warning"
+        });
+    } else {
+        const hasWishData = saveWishData.find(item => item.id === card.id);
         if (hasWishData) {
             Swal.fire({
                 title: "Already Exist",
                 icon: "warning"
-              });
-            
-        }
-       
-        else {
-            saveWishData.push(card)
-             const localWishValue = JSON.stringify(saveWishData);
-              localStorage.setItem('wishs', localWishValue)
-                Swal.fire({
-                    title: "Added Successfully",
-                    icon: "success"
-                  });
-                
-             }
-              
-             
-             
-           
-
-        
-        
-       
-        
-} 
-
-
-
-
-
-export const saveReadDataToLocalStorage =(card )=>{
-    const saveReadData = JSON.parse(localStorage.getItem('reads')) || [];
-    
-
-    
-
-    
-    const hasReadData = saveReadData.find((item) => item.id == card.id )  ;
-   
-        if (hasReadData ) {
-            Swal.fire({
-                title: "Already Exist",
-                icon: "warning"
-              });
-            
-        }
-       
-        else {
-            saveReadData.push(card)
-           
-             const localReadValue = JSON.stringify(saveReadData);
-             
-            localStorage.setItem('reads', localReadValue);
+            });
+        } else {
+            saveWishData.push(card);
+            const localWishValue = JSON.stringify(saveWishData);
+            localStorage.setItem('wishs', localWishValue);
             Swal.fire({
                 title: "Added Successfully",
                 icon: "success"
-              });
-            
+            });
         }
+    }
+}
 
-        
-        
-} 
-
-
+export const saveReadDataToLocalStorage = (card) => {
+    const saveReadData = JSON.parse(localStorage.getItem('reads')) || [];
+    const hasReadData = saveReadData.find(item => item.id === card.id);
+    if (hasReadData) {
+        Swal.fire({
+            title: "Already Exist",
+            icon: "warning"
+        });
+    } else {
+        saveReadData.push(card);
+        const localReadValue = JSON.stringify(saveReadData);
+        localStorage.setItem('reads', localReadValue);
+        Swal.fire({
+            title: "Added Successfully",
+            icon: "success"
+        });
+    }
+}
